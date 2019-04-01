6217a4931e2d33dc5bfa25a5ab5d835fe3de5684
Fyzxs <Fyzxs@users.noreply.github.com>
2019-03-31 21:03:00 -0700

    Adding initial project for If only as a guard clause
    No steps yet.

---
+++ IfOnlyAsGuardClause/IfOnlyAsGuardClauseWalkThrough/IfOnlyAsGuardClauseWalkThroughTests.cs
@@ -0,0 +1,169 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IfOnlyAsGuardClauseWalkThrough
+{
+    /*
+        If Only As a Guard Clause Refactor Kata Walk Through
+
+        This technical practice is to practice transformation for reshaping the method to not have internal branching.
+
+
+    */
+    public static class FizzBuzzUtils
+    {
+        public static void Calculate(FizzBuzz fizzBuzz)
+        {
+            if (fizzBuzz.Input % 3 == 0)
+            {
+                fizzBuzz.Result = "Fizz";
+            }
+
+            if (fizzBuzz.Input % 5 == 0)
+            {
+                if (fizzBuzz.Result == null)
+                {
+                    fizzBuzz.Result = "Buzz";
+                } else
+                {
+                    fizzBuzz.Result += "Buzz";
+                }
+            }
+
+            if (string.IsNullOrEmpty(fizzBuzz.Result))
+            {
+                fizzBuzz.Result = fizzBuzz.Input.ToString();
+            }
+        }
+    }
+
+    public class FizzBuzz
+    {
+        public int Input { get; set; }
+        public string Result { get; set; }
+    }
+
+
+    [TestClass]
+    public class IfOnlyAsGuardClauseWalkThroughTests
+    {
+        [TestMethod]
+        public void ShouldReturnString1GivenInt1()
+        {
+            //Arrange
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            fizzBuzz.Input = 1;
+            string expected = "1";
+
+            //Act
+            FizzBuzzUtils.Calculate(fizzBuzz);
+
+            //Assert
+            Assert.IsTrue(fizzBuzz.Result == expected);
+        }
+
+        [TestMethod]
+        public void ShouldReturnString2GivenInt2()
+        {
+            //Arrange
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            fizzBuzz.Input = 2;
+            string expected = "2";
+
+            //Act
+            FizzBuzzUtils.Calculate(fizzBuzz);
+
+            //Assert
+            Assert.IsTrue(fizzBuzz.Result == expected);
+        }
+
+        [TestMethod]
+        public void ShouldReturnFizzGivenInt3()
+        {
+            //Arrange
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            fizzBuzz.Input = 3;
+            string expected = "Fizz";
+
+            //Act
+            FizzBuzzUtils.Calculate(fizzBuzz);
+
+            //Assert
+            Assert.IsTrue(fizzBuzz.Result == expected);
+        }
+
+        [TestMethod]
+        public void ShouldReturnFizzGivenInt6()
+        {
+            //Arrange
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            fizzBuzz.Input = 2 * 3;
+            string expected = "Fizz";
+
+            //Act
+            FizzBuzzUtils.Calculate(fizzBuzz);
+
+            //Assert
+            Assert.IsTrue(fizzBuzz.Result == expected);
+        }
+
+        [TestMethod]
+        public void ShouldReturnBuzzGivenInt5()
+        {
+            //Arrange
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            fizzBuzz.Input = 5;
+            string expected = "Buzz";
+
+            //Act
+            FizzBuzzUtils.Calculate(fizzBuzz);
+
+            //Assert
+            Assert.IsTrue(fizzBuzz.Result == expected);
+        }
+
+        [TestMethod]
+        public void ShouldReturnBuzzGivenInt10()
+        {
+            //Arrange
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            fizzBuzz.Input = 2 * 5;
+            string expected = "Buzz";
+
+            //Act
+            FizzBuzzUtils.Calculate(fizzBuzz);
+
+            //Assert
+            Assert.IsTrue(fizzBuzz.Result == expected);
+        }
+
+        [TestMethod]
+        public void ShouldReturnFizzBuzzGivenInt15()
+        {
+            //Arrange
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            fizzBuzz.Input = 3 * 5;
+            string expected = "FizzBuzz";
+
+            //Act
+            FizzBuzzUtils.Calculate(fizzBuzz);
+
+            //Assert
+            Assert.IsTrue(fizzBuzz.Result == expected);
+        }
+
+        [TestMethod]
+        public void ShouldReturnFizzBuzzGivenInt30()
+        {
+            //Arrange
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            fizzBuzz.Input = 2 * 3 * 5;
+            string expected = "FizzBuzz";
+
+            //Act
+            FizzBuzzUtils.Calculate(fizzBuzz);
+
+            //Assert
+            Assert.IsTrue(fizzBuzz.Result == expected);
+        }
+    }
+}

474be444fc9a928fd441fdc882ec80922c7af710
Fyzxs <Fyzxs@users.noreply.github.com>
2019-03-31 22:19:33 -0700

    Adding If only as a guard clause practice
    Added rest of the solution folders for each practice

---
+++ IfOnlyAsGuardClausePractice/IfOnlyAsGuardClausePracticeTests.cs
@@ -0,0 +1,188 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IfOnlyAsGuardClausePractice
+{
+    /*
+        If Only As a Guard Clause Refactor Kata Walk Through
+
+        This technical practice is to practice transformation for reshaping the method to not have internal branching.
+    */
+    public static class FooBarUtils
+    {
+        public static void Calculate(FooBar fooBar)
+        {
+            const int fooValue = 7;
+            const int barValue = 9;
+            const string fooResult = "Foo";
+            const string barResult = "Bar";
+            const string resultNotSet = null;
+
+            if (fooBar.Input % fooValue == 0)
+            {
+                fooBar.Result = fooResult;
+            }
+
+            if (fooBar.Input % barValue == 0)
+            {
+                if (fooBar.Result == resultNotSet)
+                {
+                    fooBar.Result = barResult;
+                } else
+                {
+                    fooBar.Result += barResult;
+                }
+            }
+
+            if (string.IsNullOrEmpty(fooBar.Result))
+            {
+                fooBar.Result = fooBar.Input.ToString();
+            }
+        }
+    }
+
+    public class FooBar
+    {
+        public int Input { get; set; }
+        public string Result { get; set; }
+    }
+
+
+    [TestClass]
+    public class IfOnlyAsGuardClausePracticeTests
+    {
+        [TestMethod]
+        public void ShouldReturnString1GivenInt1()
+        {
+            //Arrange
+            FooBar fooBar = new FooBar();
+            fooBar.Input = 1;
+            string expected = "1";
+
+            //Act
+            FooBarUtils.Calculate(fooBar);
+
+            //Assert
+            Assert.IsTrue(fooBar.Result == expected);
+        }
+
+        [TestMethod]
+        public void ShouldReturnString2GivenInt2()
+        {
+            //Arrange
+            FooBar fooBar = new FooBar();
+            fooBar.Input = 2;
+            string expected = "2";
+
+            //Act
+            FooBarUtils.Calculate(fooBar);
+
+            //Assert
+            Assert.IsTrue(fooBar.Result == expected);
+        }
+
+        [TestMethod]
+        public void ShouldReturnString3GivenInt3()
+        {
+            //Arrange
+            FooBar fooBar = new FooBar();
+            fooBar.Input = 3;
+            string expected = "3";
+
+            //Act
+            FooBarUtils.Calculate(fooBar);
+
+            //Assert
+            Assert.IsTrue(fooBar.Result == expected);
+        }
+
+        [TestMethod]
+        public void ShouldReturnFooGivenInt7()
+        {
+            //Arrange
+            FooBar fooBar = new FooBar();
+            fooBar.Input = 7;
+            string expected = "Foo";
+
+            //Act
+            FooBarUtils.Calculate(fooBar);
+
+            //Assert
+            Assert.IsTrue(fooBar.Result == expected);
+        }
+
+        [TestMethod]
+        public void ShouldReturnFooGivenInt14()
+        {
+            //Arrange
+            FooBar fooBar = new FooBar();
+            fooBar.Input = 2 * 7;
+            string expected = "Foo";
+
+            //Act
+            FooBarUtils.Calculate(fooBar);
+
+            //Assert
+            Assert.IsTrue(fooBar.Result == expected);
+        }
+
+        [TestMethod]
+        public void ShouldReturnBarGivenInt9()
+        {
+            //Arrange
+            FooBar fooBar = new FooBar();
+            fooBar.Input = 9;
+            string expected = "Bar";
+
+            //Act
+            FooBarUtils.Calculate(fooBar);
+
+            //Assert
+            Assert.IsTrue(fooBar.Result == expected);
+        }
+
+        [TestMethod]
+        public void ShouldReturnBarGivenInt18()
+        {
+            //Arrange
+            FooBar fooBar = new FooBar();
+            fooBar.Input = 2 * 9;
+            string expected = "Bar";
+
+            //Act
+            FooBarUtils.Calculate(fooBar);
+
+            //Assert
+            Assert.IsTrue(fooBar.Result == expected);
+        }
+
+        [TestMethod]
+        public void ShouldReturnFooBarGivenInt63()
+        {
+            //Arrange
+            FooBar fooBar = new FooBar();
+            fooBar.Input = 7 * 9;
+            string expected = "FooBar";
+
+            //Act
+            FooBarUtils.Calculate(fooBar);
+
+            //Assert
+            Assert.IsTrue(fooBar.Result == expected);
+        }
+
+        [TestMethod]
+        public void ShouldReturnFooBarGivenInt126()
+        {
+            //Arrange
+            FooBar fooBar = new FooBar();
+            fooBar.Input = 2 * 7 * 9;
+            string expected = "FooBar";
+
+            //Act
+            FooBarUtils.Calculate(fooBar);
+
+            //Assert
+            Assert.IsTrue(fooBar.Result == expected);
+        }
+    }
+}
